191aa40cd6041b896d41df2e7524f9b4f83bdf69
Usfita Kiftiyani <cuppittt@gmail.com>
2024-09-10 13:43:58 +0900

    Update download_pretrained_models.sh

--- prepare/download_pretrained_models.sh
+++ prepare/download_pretrained_models.sh
@@ -4,19 +4,19 @@ echo -e "The pretrained models will stored in the 'checkpoints' folder\n"
 mkdir -p vae_checkpoint/
 cd vae_checkpoint/
 # vae7.ckpt
-gdown "https://drive.google.com/file/d/1Rd45YnMTQDizijhlWbhwi_fREdbKBkwl/view?usp=drive_link"
+gdown "https://drive.google.com/uc?id=1Rd45YnMTQDizijhlWbhwi_fREdbKBkwl"
 
 cd ..
 mkdir -p denoiser_checkpoint/
 cd denoiser_checkpoint/
 # denoiser.ckpt
-gdown "https://drive.google.com/file/d/1O06bwJPW3Hg0__-pwBju7CyIWNuXfl91/view?usp=drive_link"
+gdown "https://drive.google.com/uc?id=1O06bwJPW3Hg0__-pwBju7CyIWNuXfl91"
 
 cd ..
 mkdir -p motionclip_checkpoint/
 cd motionclip_checkpoint/
 # motionclip.pth.tar
-gdown "https://drive.google.com/file/d/1iqBWgM2LyGCos4aaSkGO5F0GanPmHNZ9/view?usp=drive_link"
+gdown "https://drive.google.com/uc?id=1iqBWgM2LyGCos4aaSkGO5F0GanPmHNZ9"
 
 
 echo -e "Downloading done!"
